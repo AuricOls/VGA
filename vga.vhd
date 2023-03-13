8fc507d5d83db9a0a3e8e513a4ee12ea346af48f
AuricOls <112778782+AuricOls@users.noreply.github.com>
2023-03-13 13:50:24 -0600

    Funciona

--- vga.vhd
+++ vga.vhd
@@ -1,3 +1,7 @@
+--Equipo 5
+--Auric
+--Eric
+--Demian
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
@@ -13,7 +17,7 @@ ENTITY vga IS
 		Vd: INTEGER := 525); --Vpulse+VBP+Vactive+VFP
 	PORT (
 		clk: IN STD_LOGIC; --50MHz in our board
-		update_mode:in std_logic; --button to update the image
+		update_mode:in std_logic:='1'; --button to update the image
 		sw_selector: IN STD_LOGIC_vector(1 downto 0);
 		pixel_clk: BUFFER STD_LOGIC;
 		Hsync, Vsync: BUFFER STD_LOGIC;
@@ -23,6 +27,7 @@ ENTITY vga IS
 
 ARCHITECTURE vga OF vga IS
 SIGNAL Hactive, Vactive, dena: STD_LOGIC;
+constant counter_limit  : natural := 50000000;
 BEGIN
 -------------------------------------------------------
 --Part 1: CONTROL GENERATOR
@@ -69,122 +74,104 @@ BEGIN
 	
 	VARIABLE line_counter: INTEGER RANGE 0 TO Vc;
 	Variable column_counter: integer range 0 to Hc;
-	variable left_limit:integer:=300;
-	variable right_limit:integer:=340;
-	variable up_limit:integer:=200;
-	variable down_limit:integer:=240;
+	variable left_limit:integer range Hb to 640:=300;
+	variable right_limit:integer range Hb to 640:=340;
+	variable up_limit:integer range 0 to 480:=200;
+	variable down_limit:integer range 0 to 480:=240;
 	variable step:integer:=10;
-	
-	type mode is (draw, update);
-	variable sel_mode:mode:=draw;
+	variable counter_time:integer  range 0 to 5:=0;
+
 	
 	BEGIN
---		IF (Vsync='0') THEN
---			line_counter := 0;
---		ELSIF (Hsync'EVENT AND Hsync='1') THEN
---			IF (Vactive='1') THEN
---				line_counter := line_counter + 1;
---			END IF;
---		END IF;
---		IF (Hsync='0') THEN
---			column_counter := 0;
---		ELSIF (pixel_clk'EVENT AND pixel_clk='1') THEN
---			IF (Hactive='1') THEN
---				column_counter := column_counter + 1;
---			END IF;
---		END IF;
 		
-		case sel_mode is
-			when draw=>
-				IF (Vsync='0') THEN
-					line_counter := 0;
-				ELSIF (Hsync'EVENT AND Hsync='1') THEN
-					IF (Vactive='1') THEN
-						line_counter := line_counter + 1;
-					END IF;
-				END IF;
-				IF (Hsync='0') THEN
-					column_counter := 0;
-				ELSIF (pixel_clk'EVENT AND pixel_clk='1') THEN
-					IF (Hactive='1') THEN
-						column_counter := column_counter + 1;
-					END IF;
-				END IF;
-				
-				IF (dena='1') THEN 
-				
-					if (column_counter<=left_limit or column_counter>=right_limit) then
 
-							R <= (OTHERS => '1');
-							G <= (OTHERS => '1');
-							B <= (OTHERS => '1');
+		IF (Vsync='0') THEN
+			line_counter := 0;
+		ELSIF (Hsync'EVENT AND Hsync='1') THEN
+			IF (Vactive='1') THEN
+				line_counter := line_counter + 1;
+			END IF;
+		END IF;
+		IF (Hsync='0') THEN
+			column_counter := 0;
+		ELSIF (pixel_clk'EVENT AND pixel_clk='1') THEN
+			IF (Hactive='1') THEN
+				column_counter := column_counter + 1;
+			END IF;
+		END IF;
+		
+		IF (dena='1') THEN 
+		
+			if (column_counter<=left_limit or column_counter>=right_limit) then
 
-					
-					elsif (column_counter>left_limit or column_counter<right_limit) then
-					
-						if (line_counter >up_limit and line_counter<down_limit) then
-							R <= (OTHERS => '0');
-							G <= (OTHERS => '0');
-							B <= (OTHERS => '1');
-						else
-							R <= (OTHERS => '0');
-							G <= (OTHERS => '1');
-							B <= (OTHERS => '1');
-						end if; --line counter if
-						
-					end if; -- column counter if
-							
-				ELSE
+					R <= (OTHERS => '1');
+					G <= (OTHERS => '1');
+					B <= (OTHERS => '1');
+
+			
+			elsif (column_counter>=left_limit or column_counter<=right_limit) then
+			
+				if (line_counter >up_limit and line_counter<down_limit) then
 					R <= (OTHERS => '0');
 					G <= (OTHERS => '0');
-					B <= (OTHERS => '0');
-				END IF; --if dena
-				
---				when others=>
---				null;
-				if (update_mode='0') then
-					sel_mode:=update;
+					B <= (OTHERS => '1');
 				else
-					sel_mode:=draw;
-				end if; -- update mode if
+					R <= (OTHERS => '1');
+					G <= (OTHERS => '1');
+					B <= (OTHERS => '1');
+				end if; --line counter if
 				
-			when update =>
-				sel_mode:=draw;
-----				-- 11 go up, 00 go down, 01 go left, 10 go right
---				case sw_selector is
---					when "11" =>
-----						up_limit:= up_limit-step;
-----						down_limit:= down_limit-step;
-----						if (up_limit <=0) then
-----							 up_limit:=200;
-----							 down_limit:=240;
---						end if;
---					when others=>
---						null;
-----					when "00" =>
-----						up_limit:= up_limit+step;
-----						down_limit:= down_limit+step;
-----						if (down_limit >=480) then
-----							 up_limit:=200;
-----							 down_limit:=240;
-----						end if;
-----					when "01" => --left
-----						left_limit:= left_limit-step;
-----						right_limit:= right_limit-step;
-----						if (left_limit <=0) then
-----							 left_limit:=300;
-----							 right_limit:=340;
-----						end if;
-----					when "10" => --right
-----						left_limit:= left_limit+step;
-----						right_limit:= right_limit+step;
-----						if (right_limit >=640) then
-----							 left_limit:=300;
-----							 right_limit:=340;
-----						end if;
---					end case;
---					sel_mode:=draw;
---				
-		end case;-- sel_mode case
+			end if; -- column counter if
+					
+		ELSE
+			R <= (OTHERS => '0');
+			G <= (OTHERS => '0');
+			B <= (OTHERS => '0');
+		END IF; --if dena
+		
+
+				
+			
+			--11 go up, 00 go down, 01 go left, 10 go right
+
+				if (rising_edge(clk)) then
+					if (update_mode='1') then
+						counter_time:=0;
+						end if;
+					case sw_selector is
+						when "11" =>
+							if (update_mode='0' and counter_time<10) then 
+								counter_time:=counter_time+1;
+								up_limit:= up_limit-step;
+								down_limit:= down_limit-step;
+							end if;
+							
+						when "00" =>
+							if (update_mode='0' and counter_time=0) then 
+								counter_time:=counter_time+1;
+								up_limit:= up_limit+step;
+								down_limit:= down_limit+step;
+							end if;
+							
+						when "01" =>
+							if (update_mode='0' and counter_time=0) then 
+								counter_time:=counter_time+1;
+								left_limit:= left_limit-step;
+								right_limit:= right_limit-step;
+							end if;	
+						when "10" =>
+							if (update_mode='0' and counter_time=0) then 
+								counter_time:=counter_time+1;
+								left_limit:= left_limit+step;
+								right_limit:= right_limit+step;
+							end if;							
+						when others=>
+							null;
+							
+					end case;
+				end if;
+	
+
+
 	END PROCESS;
 END vga;
